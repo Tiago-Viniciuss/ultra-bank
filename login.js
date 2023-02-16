eae8269a47225da575721f6c93000554295c29c8
Tiago Vinícius <tiagovinicyus1996@gmail.com>
2023-02-16 17:03:44 +0000

    update

--- login.js
+++ login.js
@@ -1,13 +1,11 @@
 function fazerLogin() {
-    const usuario = document.getElementById('usuario').value
+    
     const senha = document.getElementById('senha').value
 
-    if (usuario == '1234' && senha == '1234') {
+    if (senha == '1234') {
         location.href = 'home.html';
     } else {
         alert('Insira usuário e senha corretamente!')
     }
-
-
-
 }
+
