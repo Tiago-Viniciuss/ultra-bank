03a42f136f94d24534d5677d59fee7fe5c70676e
Tiago Vinícius <tiagovinicyus1996@gmail.com>
2023-02-17 20:50:51 +0000

    update

--- login.js
+++ login.js
@@ -1,15 +1,34 @@
 
 function fazerLogin() {
-    let nome = document.getElementById('usuario').value
+    let usuario = document.getElementById('usuario').value
     const senha = document.getElementById('senha').value
    
-    if (senha == '1234') {
+    if (usuario == '1234' && senha == '1234') {
         location.href = 'home.html';
     } else {
         alert('Insira usuário e senha corretamente!')
     }
 
 }
+    function gerarDados() {
+    var nomeusuario1 = document.getElementById('nome1')
+
+    var nomeusuario = nomeusuario1.value
+
+    var dados = document.getElementById('dadosdeacesso')
+
+    if (nomeusuario == 0) {
+        alert('Digite o seu nome!')
+    } else {
+        dados.innerHTML = `<p id:"dados">Olá, <strong>${nomeusuario}</strong>! <br> <br> Seja bem-vindo(a) ao Ultra Bank. <br> <br> Seus dados de acesso são: <br> <br> Usuário: <strong>1234</strong> <br> Senha: <strong>1234</strong></p>`
+
+    };
+
+   
+
+   
+    }
+   
 
 function esconderSaldo() {
     var saldo = document.getElementById('saldo')
